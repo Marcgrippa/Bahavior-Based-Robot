add26377c9795043dce600d0af58b78bdfc96ca7
Marcgrippa <hloekens@hotmail.com>
2016-11-03 13:05:02 +0100

    lagt til de nødvendige medtodene med pass

--- bbcon.py
+++ bbcon.py
@@ -2,3 +2,15 @@ class Bbcon():
 
     def __init__(self):
         pass
+
+    def add_behavior(self):
+        pass
+
+    def add_sensor(self):
+        pass
+
+    def activate_bahavior(self):
+        pass
+
+    def deactiv_behavior(self):
+        pass
