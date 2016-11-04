e4e5199fc6839b700acc4e2779bce29c243b0a99
Thomas Markussen <tsm121@gmail.com>
2016-11-04 12:08:11 +0100

    BBCON
    Neeesten ferdig

--- bbcon.py
+++ bbcon.py
@@ -1,18 +1,40 @@
+from arbitrator import Arbitrator
+
 class Bbcon():
 
     def __init__(self):
+        self.behaviors = []                     # a list of all the behavior objects used by the bbcon
+        self.active_behaviors = []              # a list of all behaviors that are currently active.
+        self.sensobs = []                       # a list of all sensory objects used by the bbcon
+        self.motobs = []                        # a list of all motor objects used by the bbcon
+        arbitrator = Arbitrator()               # the arbitrator object that will resolve actuator requests produced by the behaviors.
+
+    # append a newly-created behavior onto the behaviors list.
+    def add_behavior(self, behavior):
+
+        if behavior not in self.behaviors:
+            self.behaviors.append(behavior)
+
+    # append a newly-created sensob onto the sensobs list.
+    def add_sensor(self, sensor):
+        if sensor not in self.sensobs:
+            self.sensobs.append(sensor)
+
+    # add an existing behavior onto the active-behaviors list.
+    def activate_bahavior(self, behavior):
+        if behavior in self.behaviors:
+            self.active_behaviors.append(behavior)
+
+    # remove an existing behavior from the active behaviors list.
+    def deactive_behavior(self, behavior):
+        if behavior in self.active_behaviors:
+            self.active_behaviors.remove(behavior)
+
+    # Constitutes the core BBCON activity
+    def run_one_timestep(self):
         pass
 
-    def add_behavior(self):
-        pass
-
-    def add_sensor(self):
-        pass
-
-    def activate_bahavior(self):
-        pass
-
-    def deactiv_behavior(self):
-        pass
-
+<<<<<<< Updated upstream
 # commenting sner
+=======
+>>>>>>> Stashed changes
