330dc0c6e72bc89a2075c4a3272364d08936517f
Thomas Markussen <tsm121@gmail.com>
2016-11-08 16:14:31 +0100

    Arbitrary update1

--- arbitrator.py
+++ arbitrator.py
@@ -4,6 +4,19 @@ class Arbitrator():
         # Pointer to Bbcon-object
         bbcon_object = bbcon
 
-    def choose_action(self):
-        pass
+    def choose_action(self, behaviors):
+
+        temp_be = None
+        temp_weight = -1
+
+        #Choosing a "winning" behavior and returns that behavors motor recommendations and halt flag
+        for behavior in behaviors:
+
+            if behavior.weight > temp_weight:
+                temp_weight = behavior.weight
+                temp_be = behavior
+
+        return temp_be.motor_recommendations, temp_be.halt_request
+
+
 
